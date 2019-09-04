75281bd39cf0195e5a3786fa45ffe929e28772b5
Midstackdev <midstackdev@gmail.com>
2019-09-04 15:17:27 +0000

    Categories endpoint, Resource, Controller and tests Feature

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,4 @@
 
 use App\Models\Category;
 
-Route::get('/', function () {
-   $categories = Category::parents()->ordered()->get();
-   dd($categories);
-});
+Route::resource('categories', 'Categories\CategoryController');
