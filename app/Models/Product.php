48c6345776b1a8accf7814b32ee60685eed88197
Midstackdev <midstackdev@gmail.com>
2019-09-05 19:40:06 +0000

    Money Trait and Formatted Price

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,18 +4,19 @@ namespace App\Models;
 
 use App\Models\Category;
 use App\Models\Traits\CanBeScoped;
+use App\Models\Traits\HasPrice;
 use App\Models\ProductVariation;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Product extends Model
 {
-    use CanBeScoped;
+    use CanBeScoped, HasPrice;
 
     public function getRouteKeyName()
     {
     	return 'slug';
     }
-
 
     public function categories()
     {
