2ce49a45d86fdaadfc251dde7e0798111e7e0acc
Midstackdev <midstackdev@gmail.com>
2019-09-19 16:03:18 +0000

    Shipping Methods, Address Shipping and Policy

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\ShippingMethod;
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
 	public $timestamps = false;
     protected $fillable = ['code', 'name'];
+
+    public function shippingMethods()
+    {
+    	return $this->belongsToMany(ShippingMethod::class);
+    }
 }
---
+++ tests/Unit/Models/ShippingMethods/ShippingMethodTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Models\ShippingMethods;
+
+use App\Models\Country;
+use App\Models\ShippingMethod;
+use App\Cart\Money;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ShippingMethodTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_it_belongs_to_many_countries()
+    {
+    	$shipping = factory(ShippingMethod::class)->create();
+
+    	$shipping->countries()->attach(
+    		factory(Country::class)->create()
+    	);
+
+        $this->assertInstanceOf(Country::class, $shipping->countries->first());
+    }
+
+    public function test_it_returns_a_money_instance_for_the_price()
+    {
+    	$shipping = factory(ShippingMethod::class)->create();
+
+        $this->assertInstanceOf(Money::class, $shipping->price);
+    }
+
+    public function test_it_returns_a_formatted_price()
+    {
+    	$shipping = factory(ShippingMethod::class)->create([
+    		'price' => 0
+    	]);
+
+        $this->assertEquals($shipping->formattedPrice, '$0.00');
+    }
+}
